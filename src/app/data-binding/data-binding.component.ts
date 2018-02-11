70f55a79ed43fed400c5f8dbca4727217bcabb3e
EugeniaM <maltsevae88@gmail.com>
2018-02-11 17:55:31 +0200

    Init commit

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent {
+  public username = '';
+
+  constructor() { }
+
+  resetUsername() {
+    this.username = '';
+  }
+}
